da5fe86ba43de872be51661fb7cc572556c10f40
meyusx12-png <meyusx12@gmail.com>
2026-07-14 23:41:12 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION_STRING = os.getenv("SESSION_STRING")
+
+OWNER_NAME = "Meyus"
+BOT_NAME = "Meyus Music VC"
+COMMAND_PREFIX = "/"
